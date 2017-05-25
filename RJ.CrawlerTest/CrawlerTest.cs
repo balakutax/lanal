459caa1c2f6be6c3363f6c1b84fc387ef6c5c68c
balakutax <rzk.januar@gmail.com>
2017-05-26 01:33:09 +0700

    add CrawlerTest

---
+++ RJ.CrawlerTest/CrawlerTest.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RJ.CrawlerTest
+{
+    [TestFixture]
+    public class CrawlerTest
+    {
+        [Test]
+        public void Test_GetContentByUri(Uri uri)
+        {
+
+        }
+    }
+}
